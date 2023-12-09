3f23618689cb76c43399cfc74bb84ecbc068c0a2
zpc <zip_city@163.com>
2023-12-09 23:48:59 +0800

    import js files

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -2,7 +2,8 @@ import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap/dist/js/bootstrap.min.js';
+import "@popperjs/core/dist/umd/popper.min.js"
 
 import App from "./App";
 
