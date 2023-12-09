ba00c7ff8a3d2bb0d14994a835b0224f993a9b07
zpc <zip_city@163.com>
2023-12-09 15:45:02 +0800

    modular display(puzzle and target).select size from dropdown.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { astar, shuffle, createDest } from "./AStar";
-
-const M = 3;
-const N = 3;
+import { Dropdown, Button } from 'react-bootstrap';
 
 function exchangeWithBlank(puzzle, i, j) {
   const m = puzzle.length, n = puzzle[0].length;
@@ -27,35 +25,77 @@ function Square({value, onClick}) {
   );
 }
 
+function Options({selectedSize, onOptionClick}) {
+  return (
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        {selectedSize}
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={() => onOptionClick(3)}>3 X 3</Dropdown.Item>
+        <Dropdown.Item onClick={() => onOptionClick(4)}>4 X 4</Dropdown.Item>
+        <Dropdown.Item onClick={() => onOptionClick(5)}>5 X 5</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
+
+function Reset({onResetClick}) {
+  return <Button onClick={onResetClick}>Shuffle</Button>
+}
+
 function Reference({reference, onClick}) {
   let status = null;
   if(reference) {
     status = JSON.stringify(reference)+ ", with steps: " + reference.length ;
   }
   return (
-    <div>
-      <h1></h1>
-      <button onClick={onClick}>Reference</button>
+    <div className="status">
+      <Button onClick={onClick}>Reference</Button>
       <h4>{status}</h4>
     </div>
   );
 }
 
 export default function Board() {
-  const [squares, setSquares] = useState(shuffle(M, N));
+  const [size, setSize] = useState(3);
+  const [selectedSize, setSelectedSize] = useState("3 X 3");
+  const dest = createDest(size, size);
+  const [squares, setSquares] = useState(dest);
   const [steps, setSteps] = useState(0);
-  const dest = createDest(M, N);
+  const [status, setStatus] = useState("Target As Fallows");
   function handleClick(i, j) {
     if(isOver(squares, dest)) {
       return ;
     }
     const m = squares.length, n = squares[0].length;
     let newSquares = Array(m).fill(null).map((_, i) => squares[i].slice());
+    let newSteps = steps + 1;
+    let newStatus = "Current Steps: " + newSteps;
     exchangeWithBlank(newSquares, i, j);
     setSquares(newSquares);
-    setSteps(steps + 1);
+    setSteps(newSteps);
+    if(isOver(squares, dest)) {
+      newStatus = "Finished by steps: " + newSteps;
+    }
+    setStatus(newStatus);
   }
 
+  function handleOptions(newSize) {
+    console.log(newSize);
+    const dest = createDest(newSize, newSize);
+    setStatus("Target As Fallows");
+    setSquares(dest);
+    setSelectedSize(`${newSize} X ${newSize}`);
+    setSize(newSize);
+  }
+
+  function handleResetClick() {
+    const shuffled = shuffle(size, size);
+    setSteps(0);
+    setStatus("Current Steps: 0");
+    setSquares(shuffled);
+  }
   // http request
   const [reference, setReference] = useState(null);
   const getReference = async (puzzle) => {
@@ -78,22 +118,19 @@ export default function Board() {
       });
     };
 
-  let status = "Current Steps: " + steps;
-  if(isOver(squares, dest)) {
-    status = "Finished by steps: " + steps;
-  }
-  
   return (
     <div>
       <div className="status">{status}</div>
       <div>
         {squares.map((line, i) => 
-          <div className="board-row">
+          <div className="board-row" key={i}>
             {line.map((value, j) => 
-              <Square value={value} onClick={() => handleClick(i, j)}/> )}
+              <Square value={value} onClick={() => handleClick(i, j)} key={j}/> )}
           </div>
         )}
       </div>
+      <Options selectedSize={selectedSize} onOptionClick={handleOptions}/>
+      <Reset onResetClick={handleResetClick}/>
       <Reference reference={reference} onClick={() => getReference(squares)}/>
     </div>
   );
